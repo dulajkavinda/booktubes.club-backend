6379dbdfcd54e02f901a767f0c0ecbb802e4a196
Dulaj Kavinda <dulajkaavinda@gmail.com>
2022-03-06 12:19:54 +0530

    port change

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-require('dotenv').config();
+require("dotenv").config();
 
-const keys = require('./src/config/keys');
+const keys = require("./src/config/keys");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(cors());
 
 //Import routes
-const clubs = require('./src/routes/routes.clubs');
-const users = require('./src/routes/routes.users');
-const books = require('./src/routes/routes.books');
+const clubs = require("./src/routes/routes.clubs");
+const users = require("./src/routes/routes.users");
+const books = require("./src/routes/routes.books");
 
-app.listen(keys.PORT, () => {
-	console.log(`Server started at ${keys.PORT}`);
+app.listen(process.env.PORT || keys.PORT, () => {
+  console.log(`Server started at ${process.env.PORT}`);
 
-	//Connect to the database
-	mongoose.Promise = global.Promise;
-	mongoose.connect(
-		keys.MONGO_URI,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		},
-		(err, db) => {
-			if (!err) {
-				console.log('MongoDB Connected Successfully!!');
-			} else {
-				console.log(err);
-			}
-		},
-	);
+  //Connect to the database
+  mongoose.Promise = global.Promise;
+  mongoose.connect(
+    keys.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err, db) => {
+      if (!err) {
+        console.log("MongoDB Connected Successfully!!");
+      } else {
+        console.log(err);
+      }
+    }
+  );
 });
 
-app.get('/', (req, res) => {
-	res.send('index');
+app.get("/", (req, res) => {
+  res.send("index");
 });
 
-app.use('/clubs', clubs);
-app.use('/users', users);
-app.use('/books', books);
+app.use("/clubs", clubs);
+app.use("/users", users);
+app.use("/books", books);
