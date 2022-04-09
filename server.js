258c41c93a9467bdc77a754d46e6fa1cffd0ae82
PasinduS96 <pasindubahagya@gmail.com>
2022-04-09 22:38:12 +0530

    cors origin change

--- server.js
+++ server.js
@@ -23,6 +23,15 @@ app.use(express.static('public'));
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'https://www.booktubes.club'); // update to match the domain you will make the request from
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept',
+	);
+	next();
+});
+
 //Import routes
 const clubs = require('./src/routes/routes.clubs');
 const users = require('./src/routes/routes.users');
